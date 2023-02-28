56adc5b5da9005e93ce4c798931fe259a7089bf2
eduu17 <sranjel74@gmail.com>
2023-02-27 22:48:42 -0300

    Managing scripts | 30%

---
+++ scripts/scripts.js
@@ -0,0 +1,18 @@
+// Contador em JQuery, deve - se colocar a classe .contagem ao elemento que deve ser contado
+
+$(".contagem").each(function () {
+  $(this)
+    .prop("Counter", 0)
+    .animate(
+      {
+        Counter: $(this).text(),
+      },
+      {
+        duration: 2500,
+        easing: "swing",
+        step: function (now) {
+          $(this).text(Math.ceil(now));
+        },
+      }
+    );
+});
