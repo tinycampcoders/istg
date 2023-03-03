1e88868691200d9f41088b2c4c9bb59269cc26f7
eduu17 <sranjel74@gmail.com>
2023-03-03 00:00:48 -0300

    Website 90% done | ok

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,44 @@
 // Contador em JQuery, deve - se colocar a classe .contagem ao elemento que deve ser contado
 
 $(".contagem").each(function () {
-	$(this)
-		.prop("Counter", 0)
-		.animate(
-			{
-				Counter: $(this).text(),
-			},
-			{
-				duration: 2500,
-				easing: "swing",
-				step: function (now) {
-					$(this).text(Math.ceil(now));
-				},
-			}
-		);
+  $(this)
+    .prop("Counter", 0)
+    .animate(
+      {
+        Counter: $(this).text(),
+      },
+      {
+        duration: 2500,
+        easing: "swing",
+        step: function (now) {
+          $(this).text(Math.ceil(now));
+        },
+      }
+    );
 });
+
+$(document).on("ready", function () {
+  $(".aboutNotice--container").slick({
+    slidesToShow: 3,
+    autoplay: true,
+    slidesToScroll: 1,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  $(".aboutNotice--content").css("display", "flex");
+});
+
+AOS.init();
